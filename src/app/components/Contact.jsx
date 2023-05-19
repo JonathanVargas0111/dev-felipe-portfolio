6d56f0c2ae6249f49e2b60cc9d185ed02073784d
felipe-vargas <jonathanfelipe0111@hotmail.com>
2023-05-19 18:16:29 -0500

    Finnalizacion validacion en el formulario contact

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -1,48 +1,93 @@
+'use client'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import React from "react";
+import * as Yup from 'yup';
 
 const Contact = () => {
+  const msReque = 'Campo requerido';
+  const validationSchema = Yup.object({
+    name: Yup.string().required(msReque),
+    email: Yup.string().email('Email inválido').required('Campo requerido'),
+    message: Yup.string().required(msReque),
+  });
+
+  const handleSubmit = async (values, { setSubmitting, resetForm }) => {  
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_GET_FORM_URL, {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        // La solicitud se realizó correctamente
+        resetForm();
+        console.log("¡Datos enviados correctamente!");
+      } else {
+        // Ocurrió un error al enviar la solicitud
+        console.error("Error al enviar los datos");
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+
+    setSubmitting(false);
+  };
+
   return (
-    <section 
+    <section
       id="section-contact"
-      name="section-contac"      
-      className="w-full md:h-screen bg-gradient-to-b from-black to-gray-800 "
+      name="section-contac"
+      className="w-full md:h-screen bg-gradient-to-b from-black to-gray-800"
     >
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full text-gray-400">
         <div className="pb-8">
-          <p className="text-4xl font-bold  ">
+          <h3 className="text-4xl font-bold">
             Contact
-          </p>
-          <p className="text-red-500">En construccion</p>  
+          </h3>
         </div>
-        <div className=" flex justify-center items-center">
-          <form
-            action="https://getform.io/f/43177f3f-d4f6-4d92-b761-d113f4fe4685"
-            method="POST"
-            className=" flex flex-col w-full md:w-1/2"
+        <div className="flex justify-center items-center">
+          <Formik
+            initialValues={{ name: '', email: '', message: '' }}
+            onSubmit={handleSubmit}
+            validationSchema={validationSchema}
           >
-            <p className="py-6 m-auto text-2xl">Cuéntame de esa idea que quieres hacer realidad:</p>
-            <input
-              type="text"
-              name="name"
-              placeholder="Enter your name"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder="Enter your email"
-              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            />
-            <textarea
-              name="message"
-              placeholder="Enter your message"
-              rows="10"
-              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
-            ></textarea>
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
-              Send
-            </button>
-          </form>
+            {({ isSubmitting }) => (
+              <Form className="flex flex-col w-full md:w-1/2">
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                  className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                />
+                <ErrorMessage name="name" component="div" className="text-red-500 ml-2" />
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                />
+                <ErrorMessage name="email" component="div" className="text-red-500" />
+                <Field
+                  as="textarea"
+                  name="message"
+                  placeholder="Enter your message"
+                  rows="10"
+                  className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+                />
+                <ErrorMessage name="message" component="div" className="text-red-500" />
+                <button
+                  type="submit"
+                  className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+                  disabled={isSubmitting}
+                >
+                  Enviar
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </section>

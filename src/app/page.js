0f90bd803d1597a40bd1de92107eee89dbdc0090
JonathanVargas0111 <jonathanfelipe0111@hotmail.com>
2023-10-06 11:03:57 -0500

    change style social mendial mobile

--- src/app/page.js
+++ src/app/page.js
@@ -11,9 +11,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       {/* <NavBar />
-      <SocialLinks />
+      
       <Skills/> 
     <Portfolio/>   */}
+      <SocialLinks />
       <Header />
       <About />
       <Contact />
